72f75ad643214a86544e63393853d4984454527e
mohammedhassani <mohammedhassani1995@gmail.com>
2019-11-11 12:35:53 +0100

    Oprettelse af dokument

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require("gulp");
+const gulpfile = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const postcss = require("gulp-postcss");
@@ -10,32 +10,32 @@ const rename = require("gulp-rename");
 function styles() {
 
     return(
-        gulp.src("css/*.scss")
+        gulpfile.src("css/*.scss")
             .pipe(sourcemaps.init())
             .pipe(sass())
             .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(sourcemaps.write("."))
-            .pipe(gulp.dest("css"))
+            .pipe(gulpfile.dest("css"))
     );
 
 }
 function js() {
     return(
-        gulp.src(["js/*.js", "!js/*.min.js"])
+        gulpfile.src(["js/*.js", "!js/*.min.js"])
             .pipe(terser())
             .pipe(rename({
                 suffix: ".min"
             }))
-            .pipe(gulp.dest("js"))
+            .pipe(gulpfile.dest("js"))
     );
 }
 
 function watch(){
-    gulp.watch("css/*.scss", styles);
-    gulp.watch(["js/*.js", "!js/*.min.js"], js)
+    gulpfile.watch("css/*.scss", styles);
+    gulpfile.watch(["js/*.js", "!js/*.min.js"], js)
 }
 
-const build = gulp.parallel(styles, js);
+const build = gulpfile.parallel(styles, js);
 
 exports.styles = styles;
 exports.js = js;
